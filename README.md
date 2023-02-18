7e6fc2adce05d0590eb16fc6000509db57e8f5d9
fatonsalihu <fatonsalihu99@gmail.com>
2023-02-18 05:57:21 +0100

    Added Reset Password

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 React JS | Note App created using Firebase Authentication and Realtime Database
 
 View Link : https://note-app-e0e52.web.app/
+
+
+=======================================================
--- src/components/PasswordReset.js
+++ src/components/PasswordReset.js
@@ -29,7 +29,7 @@ function PasswordReset({ onClose }) {
           type="search"
           id="default-search"
           className="block w-80 m-2 p-2 pl-5 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Email"
+          placeholder="Please enter Email"
           value={email}
           onChange={emailChangeHandler}
           required
